5323daa4e7e5bced9e89271270f3d78af01051c2
Samuel Klepper <55067844+SamKlep@users.noreply.github.com>
2020-04-11 20:13:21 -0500

    d

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'wuvvbrb6s%+m#_5_tz3n8rduje(9&gdho4d)z_*ape(_0+i2!d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'crypto',
 ]
 
